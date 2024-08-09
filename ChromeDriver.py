1fae0836a669143981765a6c02a4e1841552eb7e
jcp99gamer <jonathan.chacko@maritimesupports.com>
2024-08-08 22:18:11 -0400

    Updated the Code from MSS Auto Mods.

--- ChromeDriver.py
+++ ChromeDriver.py
@@ -15,6 +15,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
+maximised = False
 
 def get_version_difference(version1, version2):
     """
@@ -95,65 +96,6 @@ class DriverConnector(ExeDownloader):
             sys.exit(0)
         system_version = super().get_system_version()
         print("System Arc Detected - " + chrome_version)
-        """
-        url = "https://chromedriver.chromium.org/downloads"
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, "html.parser")
-        versions = []
-        for span in soup.find_all("span", {"class": ["C9DxTc aw5Odc", "C9DxTc"]}):
-            version = span.get_text().strip()
-            if version.startswith("ChromeDriver "):
-                versions.append(version.replace("ChromeDriver ", ""))
-        version_numbers = []
-        # regex pattern for version numbers in format major.minor.patch
-        version_pattern = r'\d+\.\d+\.\d+\.\d\d'
-        # iterate over each item in the list
-        for item in versions:
-            # find all version numbers in the item
-            matches = re.findall(version_pattern, item)
-            # add all version numbers found to the list of version numbers
-            version_numbers.extend(matches)
-        available_version_numbers = list(set(version_numbers))#sorted(, reverse=True)
-        # available_version_numbers.sort(reverse=True)
-        # print("Available ChromeDriver Versions:",available_version_numbers)
-        if int(chrome_version[:3]) > 115: # npx @puppeteer/browsers install chromedriver@116.0.5845.96
-            npx_path = r"C:\Program Files\nodejs\npx.cmd"
-            result = subprocess.run([npx_path, '@puppeteer/browsers', 'install', f"chromedriver@{chrome_version}"], capture_output=True, text=True) # os.system(f"npx @puppeteer/browsers install chromedriver@{chrome_version}")
-            '''
-            os.environ["PATH"] += os.pathsep + r"C:/Program Files/nodejs/npx.cmd"
-            # Execute the command and capture the output
-            result = subprocess.run(['npx', '@puppeteer/browsers', 'install', f"chromedriver@{chrome_version}"], capture_output=True, text=True) # os.system(f"npx @puppeteer/browsers install chromedriver@{chrome_version}")
-            '''
-            # Split the output into lines
-            lines = result.stdout.splitlines()
-            # Find the line with the desired output
-            desired_line = next(line for line in lines if f"chromedriver.exe" in line) # desired_line = next(line for line in lines if f"chromedriver@{chrome_version}" in line)
-            # Extract the path
-            self.path = desired_line.rsplit('\\', 1)[0]
-            print(self.path)
-        else:
-            closest_index = super().find_closest_number_index(available_version_numbers, chrome_version)
-            closest_version_number = available_version_numbers[closest_index]
-            # print(closest_version_number)
-            # Set the URL to download the Chrome Driver
-            url = r'https://chromedriver.storage.googleapis.com/'+closest_version_number+r'/chromedriver_win32.zip'
-            # Set the path to save the downloaded file
-            download_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'chromedriver.zip')
-            # Delete the downloaded zip file if it exists
-            if os.path.exists(download_path):
-                os.remove(download_path)
-            # Download the file from the specified URL
-            urllib.request.urlretrieve(url, download_path)
-            # Extract the downloaded file to the "Child Drivers" folder
-            self.path = Path(os.path.dirname(os.path.abspath(__file__))) / "Drivers"
-            # Delete the path if Exists
-            if os.path.exists(self.path):
-                shutil.rmtree(self.path)
-            with zipfile.ZipFile(download_path, 'r') as zip_ref:
-                zip_ref.extractall(self.path)
-            # Delete the downloaded zip file
-            os.remove(download_path)
-        """
         closest_index = super().find_closest_number_index(available_version_numbers, chrome_version)
         closest_version_number = available_version_numbers[closest_index]
         print("Download Version - ",closest_version_number)
@@ -177,28 +119,29 @@ class DriverConnector(ExeDownloader):
         with zipfile.ZipFile(download_path, 'r') as zip_ref:
             zip_ref.extractall(self.path)
         os.remove(download_path) # Delete the downloaded zip file
-    def DriverExe(self):
+    def DriverExe(self, args = '--headless'): # Extract the downloaded file to the "Child Drivers" folder
         extract_path = Path(os.path.dirname(os.path.abspath(__file__))) / "Drivers" # Extract the downloaded file to the "Child Drivers" folder
-        chromedriver_path = str(extract_path)+r"\chromedriver.exe" # Initialize the variable for the chromedriver path
+        driver_path = str(extract_path)+r"\chromedriver.exe" # Initialize the variable for the chromedriver path
         for root, dirs, files in os.walk(extract_path): # Walk through all directories and subdirectories
             if 'chromedriver.exe' in files:
-                chromedriver_path = Path(root) / 'chromedriver.exe'
+                driver_path = Path(root) / 'chromedriver.exe'
                 break  # Stop once we've found the chromedriver
             else:
-                # chromedriver_path = None
+                # driver_path = None
                 continue
-        print(chromedriver_path)
-        webdriver_service = Service(chromedriver_path) # Specify the path to the Chrome web driver executable
+        print(driver_path)
+        webdriver_service = Service(driver_path) # Specify the path to the Chrome web driver executable
         website_options = Options() # Set up Selenium to use Chrome browser
-        website_options.add_argument('--headless')  # Use headless mode to avoid opening a visible browser window
+        website_options.add_argument(args)  # Use headless mode to avoid opening a visible browser window
+        # website_options.add_argument("--log-level=3")  # Suppress console logs
         self.driver = webdriver.Chrome(service=webdriver_service, options=website_options) # Create a new instance of the Chrome driver
         # driver = webdriver.Chrome(service=webdriver_service)
         return self.driver
-def DriverChecker():
-    browser = "chrome"
+def DriverChecker(browser = "chrome"):
     if(browser == "chrome"):
         obj = DriverConnector()
-        driver = obj.DriverExe()
+        argx = '--headless' if maximised != True else "--start-maximized"
+        driver = obj.DriverExe(argx)
         return driver
     else:
         pass
